664eac9cbe085e616282d296cf0ec9e6c31e5d57
Akshar Patel <akshar.patel.47@gmail.com>
2016-10-17 19:02:34 +0530

    scaffold UOTM client

---
+++ udacity-OnTheMap/UOTMClient.swift
@@ -0,0 +1,13 @@
+//
+//  UOTMClient.swift
+//  udacity-OnTheMap
+//
+//  Created by Techniexe on 17/10/16.
+//  Copyright © 2016 Ekantik Tech Studio. All rights reserved.
+//
+
+import Foundation
+
+class UOTM {
+  static let shared = UOTM()
+}
---
+++ udacity-OnTheMap/UOTMConstants.swift
@@ -0,0 +1,16 @@
+//
+//  UOTMConstants.swift
+//  udacity-OnTheMap
+//
+//  Created by Techniexe on 17/10/16.
+//  Copyright © 2016 Ekantik Tech Studio. All rights reserved.
+//
+
+import Foundation
+
+extension UOTM {
+  struct Methods {
+    let studentLocation = "https://parse.udacity.com/parse/classes/StudentLocation"
+    let session = "https://www.udacity.com/api/session"
+  }
+}
