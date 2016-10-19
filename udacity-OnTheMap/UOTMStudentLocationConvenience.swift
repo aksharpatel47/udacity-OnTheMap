7c5b1d595e29d6c4df9f7bfc028eb387cbc0fed1
Akshar Patel <akshar.patel.47@gmail.com>
2016-10-19 11:29:48 +0530

    scaffold student location network request convenice functions

---
+++ udacity-OnTheMap/UOTMStudentLocationConvenience.swift
@@ -0,0 +1,27 @@
+//
+//  UOTMStudentLocationConvenience.swift
+//  udacity-OnTheMap
+//
+//  Created by Techniexe on 19/10/16.
+//  Copyright © 2016 Ekantik Tech Studio. All rights reserved.
+//
+
+import Foundation
+
+extension UOTMClient {
+  func getStudentLocations() {
+    
+  }
+  
+  func getStudentLocation(uniqueKey: String) {
+    
+  }
+  
+  func postStudentLocation() {
+    
+  }
+  
+  func updateStudentLocation() {
+    
+  }
+}
