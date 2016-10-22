9168c92fc247ac620eb02b57ac6bc91d00345b4d
Akshar Patel <akshar.patel.47@gmail.com>
2016-10-22 10:02:30 +0530

    refactor: change StudentLocationPin props to let

--- udacity-OnTheMap/StudentLocationPin.swift
+++ udacity-OnTheMap/StudentLocationPin.swift
@@ -10,9 +10,9 @@ import Foundation
 import MapKit
 
 class StudentLocationPin: NSObject, MKAnnotation {
-  var title: String?
-  var subtitle: String?
-  var coordinate: CLLocationCoordinate2D
+  let title: String?
+  let subtitle: String?
+  let coordinate: CLLocationCoordinate2D
   
   init(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D) {
     self.title = title
